d299e4d5e10e1e5194d6a54d5c7c914a99a468df
Vladimir <128.valdemar@gmail.com>
2023-06-22 20:41:00 +0300

    Section1

--- src/components/section1/section1.jsx
+++ src/components/section1/section1.jsx
@@ -9,7 +9,7 @@ export default function Section1() {
     return (
         <div className="section1">
             <div className="text-block">
-                <div className="choice">⭐ выбор разработчиков</div>
+                <div className="choice">⭐ Выбор разработчиков</div>
                 <div className="info">
                     <div className="title">Мощный игровой<br />хостинг</div>
                     <div className="text">Создайте свой сервер SA:MP, CR:MP, MTA, MineCraft,<br />
